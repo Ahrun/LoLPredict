cf53535d8eb53eebb02a598b4d4bc656164df36f
Ahrun <williams.5481@osu.edu>
2018-03-29 00:43:47 -0400

    basic data scraping,

---
+++ LoLPredict/LoLPredict/Program.cs
@@ -0,0 +1,42 @@
+﻿using DataScraper;
+using RiotSharp.MatchEndpoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoLPredict
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            RiotAPIService riotApi = new RiotAPIService("RGAPI-d083dda5-f726-46e8-9b79-315652a446c0");
+            var challengerPlayers = riotApi.GetChallengerLeague();
+            var masterPlayers = riotApi.GetMasterLeague();
+            List<long> matchIds = new List<long>();
+
+            foreach (var challengerPlayer in challengerPlayers)
+            {
+                var playerMatches = riotApi.GetRecentMatches(challengerPlayer);
+                foreach(var playerMatch in playerMatches)
+                {
+                    matchIds.Add(playerMatch.GameId);
+                }
+            }
+            foreach(var masterPlayer in masterPlayers)
+            {
+                var playerMatches = riotApi.GetRecentMatches(masterPlayer);
+                foreach (var playerMatch in playerMatches)
+                {
+                    matchIds.Add(playerMatch.GameId);
+                }
+            }
+            matchIds = matchIds.Distinct().ToList();
+            List<Match> matches = new List<Match>();
+            foreach(long matchId in matchIds){
+                matches.Add(riotApi.GetMatch(matchId));
+            }
+        }
+    }
+}
---
+++ LoLPredict/LoLPredict/RiotAPIService.cs
@@ -0,0 +1,56 @@
+﻿using RiotSharp;
+using RiotSharp.LeagueEndpoint;
+using RiotSharp.MatchEndpoint;
+using System;
+using System.Collections.Generic;
+
+namespace DataScraper
+{
+    public class RiotAPIService
+    {
+        RiotApi _riotApi;
+        int limiter = 0;
+        public RiotAPIService(string apiKey)
+        {
+            _riotApi = RiotApi.GetDevelopmentInstance(apiKey);
+        }
+
+        public List<LeaguePosition> GetChallengerLeague()
+        {
+            RateLimiter();
+            var challenger = _riotApi.GetChallengerLeague(RiotSharp.Misc.Region.na, "RANKED_SOLO_5x5");
+            return challenger.Entries;
+        }
+
+        public List<LeaguePosition> GetMasterLeague()
+        {
+            RateLimiter();
+            var master = _riotApi.GetMasterLeague(RiotSharp.Misc.Region.na, "RANKED_SOLO_5x5");
+            return master.Entries;
+        }
+
+        public List<MatchReference> GetRecentMatches(LeaguePosition leaguePosition)
+        {
+            RateLimiter();
+            var accountId = _riotApi.GetSummonerBySummonerId(RiotSharp.Misc.Region.na, Int64.Parse(leaguePosition.PlayerOrTeamId)).AccountId;
+            RateLimiter();
+            return _riotApi.GetRecentMatches(RiotSharp.Misc.Region.na, accountId);
+        }
+
+        public Match GetMatch(long matchId)
+        {
+            RateLimiter();
+            return _riotApi.GetMatch(RiotSharp.Misc.Region.na, matchId);
+        }
+
+        private void RateLimiter()
+        {
+            if (limiter > 50)
+            {
+                System.Threading.Thread.Sleep(300000);
+                limiter = 0;
+            }
+            limiter++;
+        }
+    }
+}
