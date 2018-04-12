43b177b16b5bab8a9d6c68462a98f6aa604090de
Ahrun <ahrun101@gmail.com>
2018-04-12 11:58:25 -0400

    Updated models

---
+++ LoLPredict/LoLPredict/Models/Mastery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataScraper.Models
+{
+    public class Mastery
+    {
+        public int masteryId { get; set; }
+        public int rank { get; set; }
+    }
+}
---
+++ LoLPredict/LoLPredict/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataScraper.Models
+{
+    public class Player
+    {
+        public string currentPlatformId { get; set; }
+        public string summonerName { get; set; }
+        public string matchHistoryUri { get; set; }
+        public string platformId { get; set; }
+        public long currentAccountId { get; set; }
+        public int profileIcon { get; set; }
+        public long summonerId { get; set; }
+        public long accountId { get; set; }
+    }
+}
