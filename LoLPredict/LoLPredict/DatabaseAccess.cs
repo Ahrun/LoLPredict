c5e3ada19a094e8512b97cf1512c5d613c41c90a
Ahrun <williams.5481@osu.edu>
2018-04-12 21:44:19 -0500

    Database access changes

--- LoLPredict/LoLPredict/DatabaseAccess.cs
+++ LoLPredict/LoLPredict/DatabaseAccess.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SQLite;
+using System.IO;
 
 namespace DataScraper
 {
     public class DatabaseAccess
     {
-        //TODO: This
+        SQLiteConnection _sqlConnection;
+
+        public DatabaseAccess()
+        {
+            _sqlConnection.ConnectionString = "Data source=Database.sqlite";
+            if (!File.Exists("Database.sqlite"))
+            {
+                CreateDatabase();
+            }
+        }
+
+        private void CreateDatabase()
+        {
+            SQLiteConnection.CreateFile("Database.sqlite");
+            _sqlConnection.ConnectionString = "Data source=Database.sqlite";
+            _sqlConnection.Open();
+            CreateTables();
+            _sqlConnection.Close();
+        }
+
+        private void CreateTables()
+        {
+
+        }
     }
 }
